93348e237b13f50c95ed081f53a9d1a2e2b2ed5b
miki <mikki.stad@gmail.com>
2021-04-03 22:59:30 +0900

    init unity project files

---
+++ Assets/Package/Runtime/RenderTextureDirect.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace dev.kemomimi.renderTextureDirect
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+
+    public class RenderTextureDirect : MonoBehaviour
+    {
+        [SerializeField] private RenderTexture renderTexture;
+
+        private Camera cam;
+        private Material rtAttachedMat;
+
+        private void OnEnable()
+        {
+            cam = GetComponent<Camera>();
+            cam.targetTexture = null;
+            cam.clearFlags = CameraClearFlags.SolidColor;
+            cam.backgroundColor = Color.clear;
+            cam.cullingMask = 0;
+            cam.nearClipPlane = 0.1f;
+            cam.farClipPlane = 0.2f;
+
+            if (rtAttachedMat != null)
+                return;
+
+            rtAttachedMat = new Material(Shader.Find("RenderTextureDirect/Attach"));
+            rtAttachedMat.SetTexture("_RenderTex", renderTexture);
+        }
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            if (rtAttachedMat == null)
+                return;
+
+            Graphics.Blit(source, destination, rtAttachedMat);
+        }
+    }
+}
